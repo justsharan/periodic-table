c29e5ce4f84cf99dde03e5ea24c2bb279652dfb8
SinistreCyborg <sinistrecyborg@yahoo.com>
2020-12-01 18:25:07 -0500

    feat: Added Element component

--- src/App.svelte
+++ src/App.svelte
@@ -1,2 +1,32 @@
-<h1>Hello World!</h1>
+<script>
+  import Element from './Element.svelte'
+
+  const Hydrogen = {
+    atomic: 1,
+    symbol: "H",
+    name: "Hydrogen"
+  }
+
+  const Helium = {
+    atomic: 2,
+    symbol: "He",
+    name: "Helium"
+  }
+</script>
+
+<main>
+
+    <Element {...Hydrogen} />
+    <Element {...Helium} />
+
+</main>
+
+<style>
+
+  main {
+    display: grid;
+    grid-template-columns: repeat(17, auto) 1fr;
+  }
+
+</style>
 
---
+++ src/Element.svelte
@@ -0,0 +1,46 @@
+<script>
+  export let atomic
+  export let symbol
+  export let name
+</script>
+
+<main id={name.toLowerCase()}>
+  <div id="atomic">{atomic}</div>
+  <div id="symbol">{symbol}</div>
+  <div id="name">{name}</div>
+</main>
+
+<style>
+
+  main {
+    width: 5vw;
+    height: 5vw;
+    padding: 2px;
+
+    border: 1px solid;
+    border-collapse: collapse;
+    overflow: hidden;
+
+    font-family: -apple-system, Helvetica, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  #atomic {
+    float: left;
+    font-size: 0.5vw;
+    position: absolute;
+  }
+
+  #symbol {
+    font-weight: bold;
+    text-align: center;
+    margin-top: 0.5vw;
+    font-size: 2.5vw;
+  }
+
+  #name {
+    font-size: 0.8vw;
+    text-align: center;
+  }
+
+</style>
+
