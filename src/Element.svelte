896141ca6932e7508269ba60ac5f55fe2b724801
SinistreCyborg <sinistrecyborg@yahoo.com>
2020-12-02 08:54:45 -0500

    feat: Added colors

--- src/Element.svelte
+++ src/Element.svelte
@@ -2,10 +2,11 @@
   export let atomic
   export let symbol
   export let name
+  export let type
   export let atomic_mass
 </script>
 
-<section id={name.toLowerCase()}>
+<section id={name.toLowerCase()} class={type}>
   <div id="atomic">{atomic}</div>
   <div id="symbol">{symbol}</div>
   <div id="name">{name}</div>
@@ -24,7 +25,7 @@
     overflow: hidden;
 
     font-family: -apple-system, Helvetica, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    color: #eeeeee;
+    color: hsla(0,0%,100%,.55);
   }
 
   #atomic {
@@ -73,6 +74,42 @@
   #space {
     grid-column-start: -2;
     border: none;
+  }
+
+  .alkali {
+    background-color: rgba(114,47,61,.8);
+  }
+
+  .alkaline-earth {
+    background-color: rgba(95,70,55,.8);
+  }
+
+  .transition {
+    background-color: rgba(58,70,90,.8);
+  }
+
+  .post-transition {
+    background-color: rgba(28,100,73,.8);
+  }
+
+  .metalloid {
+    background-color: rgba(21,84,100,.8);
+  }
+
+  .nonmetal {
+    background-color: rgba(77,74,150,.8);
+  }
+
+  .noble-gas {
+    background-color: rgba(87,49,126,.8);
+  }
+
+  .lanthanide {
+    background-color: rgba(74,57,110,.8);
+  }
+
+  .actinide {
+    background-color: rgba(64,37,80,.8);
   }
 
 </style>
